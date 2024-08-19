54a64f22d27d508b0e4d95604a8849d3b1142938
ferneynava <ferneynava@gmail.com>
2024-08-19 17:14:55 -0500

    :memo: [Add] documentation
    [Add] documentation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # -starting-style-prueba
 Nuevo @starting-style para efectos de entrada y salida de etapa 
+
+ <img src="./starting-style.gif">
---
+++ app.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('#button');
+const dialog = document.querySelector('dialog');
+
+button.addEventListener('click', () => {
+    dialog.showModal();
+    });
+    
